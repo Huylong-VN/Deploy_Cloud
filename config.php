a45973eaa21fbda1461af62c2f1d4a7d5b8339e8
adultsforfuture <adultsforfuture@gmail.com>
2021-06-30 08:45:18 +0700

    ok

--- config.php
+++ config.php
@@ -1,4 +1,12 @@
 <?php
-$con=mysqli_connect("52.6.114.59","ec2-user","123@123a","anhhuy");
-mysqli_set_charset($con,'UTF8')
+	$server = "54.236.43.60";
+	$user = "huy";
+	$pass = "123";
+	$database = "anhhuy";
+	$connect = mysqli_connect($server, $user, $pass, $database); 
+	mysqli_set_charset($connect, 'UTF8');
+	if (!$connect) {
+		die("Connect Failed:".mysqli_connect_error());
+		# code...
+	}
 ?>
